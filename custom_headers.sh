0f2d7378320db6ba57adf6edf743afbf889d4a6d
Enrique Belarte Luque <ebelarte@redhat.com>
2024-05-31 07:52:41 -0500

    Add DKMS reconfig

--- custom_headers.sh
+++ custom_headers.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 
-DKMS_CONF_PATH="/var/tmp/rebuild/usr/src/habanalabs-${DRIVER_VERSION}/dkms.conf"
+#DKMS_CONF_PATH="/var/tmp/rebuild/usr/src/habanalabs-${DRIVER_VERSION}/dkms.conf"
+DKMS_CONF_PATH=$(find /usr/src -name dkms.conf)
+
 
 if [ -f "$DKMS_CONF_PATH" ]; then
     sed -i "s|$kernelver|$KERNELVER|g" "$DKMS_CONF_PATH"
